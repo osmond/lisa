90e1651ca7589c899194adc70add55e1418e981f
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-13 09:32:15 -0500

    feat: open lightbox in plant gallery

--- src/pages/__tests__/Gallery.test.jsx
+++ src/pages/__tests__/Gallery.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { MemoryRouter, Routes, Route } from 'react-router-dom'
 import Gallery from '../Gallery.jsx'
 import plants from '../../plants.json'
@@ -18,4 +18,22 @@ test('renders gallery images for plant', () => {
 
   const images = screen.getAllByAltText(plant.name)
   expect(images.length).toBeGreaterThanOrEqual(plant.photos.length)
+})
+
+test('lightbox opens when image clicked', () => {
+  const plant = plants[0]
+  render(
+    <PlantProvider>
+      <MemoryRouter initialEntries={[`/plant/${plant.id}/gallery`]}>
+        <Routes>
+          <Route path="/plant/:id/gallery" element={<Gallery />} />
+        </Routes>
+      </MemoryRouter>
+    </PlantProvider>
+  )
+
+  const img = screen.getAllByAltText(plant.name)[0]
+  fireEvent.click(img)
+
+  expect(screen.getByRole('dialog')).toBeInTheDocument()
 })
