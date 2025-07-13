d0032b0f8909dbf30da4481470f32f0fdc5483f0
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-13 09:25:42 -0500

    Add gallery link to BottomNav and test

--- src/components/__tests__/BottomNav.test.jsx
+++ src/components/__tests__/BottomNav.test.jsx
@@ -2,14 +2,24 @@ import { render } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import BottomNav from '../BottomNav.jsx'
 
- test('all icons are aria-hidden', () => {
-   const { container } = render(
-     <MemoryRouter>
-       <BottomNav />
-     </MemoryRouter>
-   )
-   const svgs = container.querySelectorAll('svg')
-   svgs.forEach(svg => {
-     expect(svg).toHaveAttribute('aria-hidden', 'true')
-   })
- })
+test('all icons are aria-hidden', () => {
+  const { container } = render(
+    <MemoryRouter>
+      <BottomNav />
+    </MemoryRouter>
+  )
+  const svgs = container.querySelectorAll('svg')
+  svgs.forEach(svg => {
+    expect(svg).toHaveAttribute('aria-hidden', 'true')
+  })
+})
+
+test('renders gallery link', () => {
+  const { container } = render(
+    <MemoryRouter>
+      <BottomNav />
+    </MemoryRouter>
+  )
+  const galleryLink = container.querySelector('a[href="/gallery"]')
+  expect(galleryLink).not.toBeNull()
+})
