0c1e6e780c7ea69a457d1a80fb244a1d4e64f7a8
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-13 08:26:35 -0500

    Switch icons to phosphor and radix

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,77 +1,29 @@
 import { NavLink } from 'react-router-dom'
+import {
+  CheckCircledIcon,
+  HomeIcon,
+  ListBulletIcon,
+  PersonIcon,
+  PlusIcon,
+} from '@radix-ui/react-icons'
 
-function IconWrapper({ children, className }) {
-  return (
-    <svg
-      className={`w-6 h-6 ${className}`}
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      aria-hidden="true"
-    >
-      {children}
-    </svg>
-  )
+const iconProps = {
+  className: 'w-6 h-6',
+  'aria-hidden': 'true',
 }
 
-const HomeIcon = () => (
-  <IconWrapper>
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-    />
-  </IconWrapper>
-)
-
-const ListIcon = () => (
-  <IconWrapper>
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-    />
-  </IconWrapper>
-)
-
-const CheckCircleIcon = () => (
-  <IconWrapper>
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-    />
-  </IconWrapper>
-)
-
-const PlusCircleIcon = () => (
-  <IconWrapper>
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-    />
-  </IconWrapper>
-)
-
-const UserIcon = () => (
-  <IconWrapper>
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-    />
-  </IconWrapper>
-)
+const HomeIconComponent = () => <HomeIcon {...iconProps} />
+const ListIcon = () => <ListBulletIcon {...iconProps} />
+const CheckIcon = () => <CheckCircledIcon {...iconProps} />
+const AddIcon = () => <PlusIcon {...iconProps} />
+const UserIcon = () => <PersonIcon {...iconProps} />
 
 export default function BottomNav() {
   const items = [
-    { to: '/', label: 'Home', icon: HomeIcon },
+    { to: '/', label: 'Home', icon: HomeIconComponent },
     { to: '/myplants', label: 'My Plants', icon: ListIcon },
-    { to: '/tasks', label: 'Tasks', icon: CheckCircleIcon },
-    { to: '/add', label: 'Add', icon: PlusCircleIcon },
+    { to: '/tasks', label: 'Tasks', icon: CheckIcon },
+    { to: '/add', label: 'Add', icon: AddIcon },
     { to: '/settings', label: 'Profile', icon: UserIcon },
   ]
 
