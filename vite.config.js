2ff2b1b1013d4e36da4d922159c2dcafbff18aef
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-13 17:33:07 -0500

    fix env loading

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const basePath = process.env.VITE_BASE_PATH || '/'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const basePath = env.VITE_BASE_PATH || '/'
 
-export default defineConfig({
-  base: basePath,
-  define: {
-    'process.env.VITE_WEATHER_API_KEY': JSON.stringify(
-      process.env.VITE_WEATHER_API_KEY
-    ),
-    'process.env.VITE_BASE_PATH': JSON.stringify(basePath),
-    'import.meta.env.VITE_BASE_PATH': JSON.stringify(basePath),
-  },
-  plugins: [react()],
+  return {
+    base: basePath,
+    define: {
+      'process.env.VITE_WEATHER_API_KEY': JSON.stringify(env.VITE_WEATHER_API_KEY),
+      'process.env.VITE_BASE_PATH': JSON.stringify(basePath),
+      'import.meta.env.VITE_BASE_PATH': JSON.stringify(basePath),
+    },
+    plugins: [react()],
+  }
 })
