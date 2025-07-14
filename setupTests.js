1f94247aa565bcda8bb9505f9e464b121f3ba1db
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-14 12:55:41 -0500

    chore(test): silence react router warnings

--- setupTests.js
+++ setupTests.js
@@ -1,1 +1,19 @@
 import '@testing-library/jest-dom';
+
+// Silence React Router "Future Flag" warnings during tests
+const originalWarn = console.warn;
+beforeAll(() => {
+  jest.spyOn(console, 'warn').mockImplementation((...args) => {
+    if (
+      typeof args[0] === 'string' &&
+      args[0].includes('React Router Future Flag Warning')
+    ) {
+      return;
+    }
+    originalWarn.call(console, ...args);
+  });
+});
+
+afterAll(() => {
+  console.warn.mockRestore();
+});
