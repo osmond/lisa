69b8812ae98a310494185086db4457c08c7c17dc
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-14 11:48:31 -0500

    style: color icons and add button feedback

--- src/components/ActionIcons.jsx
+++ src/components/ActionIcons.jsx
@@ -8,18 +8,31 @@ import {
 } from 'phosphor-react'
 
 const iconProps = {
-  className: 'w-6 h-6 text-gray-500 dark:text-gray-400',
+  className: 'w-6 h-6',
   'aria-hidden': 'true',
 }
-export const WaterIcon = () => <Drop {...iconProps} />
+export const WaterIcon = () => (
+  <Drop {...iconProps} className={`text-blue-600 ${iconProps.className}`} />
+)
 
-export const FertilizeIcon = () => <Sun {...iconProps} />
+export const FertilizeIcon = () => (
+  <Sun {...iconProps} className={`text-yellow-600 ${iconProps.className}`} />
+)
 
-export const RotateIcon = () => <ArrowCounterClockwise {...iconProps} />
+export const RotateIcon = () => (
+  <ArrowCounterClockwise
+    {...iconProps}
+    className={`text-purple-600 ${iconProps.className}`}
+  />
+)
 
-export const NoteIcon = () => <Note {...iconProps} />
+export const NoteIcon = () => (
+  <Note {...iconProps} className={`text-gray-600 ${iconProps.className}`} />
+)
 
-export const LogIcon = () => <Notebook {...iconProps} />
+export const LogIcon = () => (
+  <Notebook {...iconProps} className={`text-green-600 ${iconProps.className}`} />
+)
 
 export const icons = {
   Water: WaterIcon,
