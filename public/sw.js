b52a78f19407511dec6178c36e29d369c0bb6ca3
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-14 22:54:44 -0500

    Add PWA manifest and service worker

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'kaymaria-cache-v1'
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/favicon.svg'
+]
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting()
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  )
+})
