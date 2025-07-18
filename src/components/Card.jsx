37b874ec9b2b1ae2d88e6a4b86be34796f82adb6
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-18 17:14:40 -0500

    Refine UI styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,9 @@
 export default function Card({ as: Component = 'div', className = '', children, ...props }) {
   return (
-    <Component className={`bg-white dark:bg-gray-700 rounded-2xl shadow-sm p-4 ${className}`} {...props}>
+    <Component
+      className={`bg-white dark:bg-gray-700 rounded-2xl shadow-sm border border-gray-200 dark:border-gray-600 p-4 ${className}`}
+      {...props}
+    >
       {children}
     </Component>
   )
