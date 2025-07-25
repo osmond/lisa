804b75ec1b4dee7b27709c971429207c7e06804b
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-25 06:28:44 -0500

    fix: avoid stray zero on care plan tab

--- src/pages/PlantDetail.jsx
+++ src/pages/PlantDetail.jsx
@@ -462,7 +462,7 @@ export default function PlantDetail() {
                 )}
               </>
             )}
-            {(plant.carePlan || plant.waterPlan?.interval) && (
+            {Boolean(plant.carePlan || plant.waterPlan?.interval) && (
               <Link
                 to={`/plant/${plant.id}/edit-care-plan`}
                 className="inline-block px-3 py-1.5 text-sm font-semibold text-white bg-green-600 rounded-full"
