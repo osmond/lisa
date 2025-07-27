4ab855921fd6a4bc1321b43acd4c801b9d7998f8
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-26 19:32:19 -0500

    feat: fallback to wikipedia images

--- setupTests.js
+++ setupTests.js
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 
 // Suppress React Router future flag warnings during tests
 const originalWarn = console.warn;
+const nativeFetch = global.fetch;
 beforeAll(() => {
   jest.spyOn(console, 'warn').mockImplementation((...args) => {
     if (
@@ -12,8 +13,26 @@ beforeAll(() => {
     }
     originalWarn.call(console, ...args);
   });
+  if (!global.fetch) {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({ ok: true, json: () => Promise.resolve({}) })
+    );
+  }
+});
+
+beforeEach(() => {
+  if (!global.fetch) {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({ ok: true, json: () => Promise.resolve({}) })
+    );
+  }
 });
 
 afterAll(() => {
   console.warn.mockRestore();
+  if (nativeFetch) {
+    global.fetch = nativeFetch;
+  } else {
+    delete global.fetch;
+  }
 });
