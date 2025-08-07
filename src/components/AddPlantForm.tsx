ccd0b9d9f2497db73212f3f7bba2ac519f948e1a
Jon Osmond <jonathan.osmond@gmail.com>
2025-08-07 07:25:30 -0500

    refactor: update add plant button

--- src/components/AddPlantForm.tsx
+++ src/components/AddPlantForm.tsx
@@ -140,13 +140,11 @@ export default function AddPlantForm({
       </section>
 
       <button
-
-        type="button"
-        onClick={handleSubmit(onSubmit)}
+        type="submit"
+        disabled={submitDisabled}
         className="px-4 py-2 bg-green-600 text-white rounded"
       >
-        {mode === 'add' ? 'Add Plant' : 'Save Changes'}
-
+        {submitLabel ?? (mode === 'add' ? 'Add Plant' : 'Save Changes')}
       </button>
     </form>
   )
